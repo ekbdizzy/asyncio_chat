dbdfb3169d6e9054497406e9cc5c2bb5867d51ca
Aleksey Koshkin <ekbdizzy@MacBook-Pro-Aleksej.local>
2020-12-24 00:20:06 +0300

    Read all messages

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ async def tcp_echo_client(message):
         host=HOST, port=PORT
     )
 
+    # print(f'Send {message!r}')
+    # writer.write(message.encode())
 
-    print(f'Send {message!r}')
-    writer.write(message.encode())
+    data = await reader.readline()
+    print(f"{data.decode()!r}")
 
-    data  = await reader.read(100)
-    print(f"Received {data.decode()!r}")
-
-    print("Close the connection")
-    writer.close()
-
-asyncio.run(tcp_echo_client("Hello"))
+    # print("Close the connection")
+    # writer.close()
 
 
+if __name__ == "__main__":
+    while True:
+        asyncio.run(tcp_echo_client(""))
