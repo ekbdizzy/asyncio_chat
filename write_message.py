6a13bd165aca31eca6b6d0243193e9267c00c452
Aleksey Koshkin <ekbdizzy@yandex.ru>
2022-06-06 18:22:16 +0600

    Setup logging

--- write_message.py
+++ write_message.py
@@ -1,13 +1,12 @@
 import asyncio
 import argparse
+import logging
 
 from environ import Env
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-m', '--message', dest='message', help='Message text')
-
 env = Env()
 env.read_env()
+logger = logging.getLogger('sender')
 
 
 async def write_message(message):
@@ -15,17 +14,28 @@ async def write_message(message):
         host=env('HOST'), port=5050
     )
 
-    await reader.readline()
+    text = await reader.readline()
+    logger.info(text.decode())
+
     writer.write(f"{env('ACCOUNT_TOKEN')}\n".encode())
     await writer.drain()
 
-    phrase = await reader.readline()
-    print(phrase)
+    text = await reader.readline()
+    logger.info(text.decode())
 
     writer.write(f'{message}\n\n'.encode())
     await writer.drain()
 
 
-if __name__ == '__main__':
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--message', dest='message', help='Message text')
     args = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG)
+
     asyncio.run(write_message(args.message))
+
+
+if __name__ == '__main__':
+    main()
