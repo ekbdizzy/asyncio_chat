de06dcc00a71da49820da1f81bd41afaeedb77c7
Aleksey Koshkin <ekbdizzy@yandex.ru>
2023-10-21 19:58:18 +0700

    Add chat history functionality

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ import argparse
 import asyncio
 import time
 
+import aiofiles
 from environ import Env
 
 from streaming_tools import open_connection, add_timestamp
 import gui
 
-loop = asyncio.get_event_loop()
-
 messages_queue = asyncio.Queue()
 sending_queue = asyncio.Queue()
+saving_queue = asyncio.Queue()
 status_updates_queue = asyncio.Queue()
+
+
+async def load_history(filepath: str, queue: asyncio.Queue):
+    """Load chat history and place it in the beginning of messages."""
+    async with aiofiles.open(filepath) as file:
+        contents = await file.read()
+        await queue.put(contents.strip())
 
 
 async def generate_msgs(queue: asyncio.Queue):
@@ -20,9 +27,12 @@ async def generate_msgs(queue: asyncio.Queue):
         await asyncio.sleep(1)
 
 
-async def greeting(queue: asyncio.Queue):
-    queue.put_nowait('Hi there')
-    queue.put_nowait('Welcome to the chat')
+async def save_messages(filepath: str, queue: asyncio.Queue):
+    """Save messages from the queue (which had been read from server) to the chat history file."""
+    while True:
+        async with aiofiles.open(filepath, 'a') as file:
+            phrase = await queue.get()
+            await file.write(f'{phrase}\n')
 
 
 async def read_msgs(host: str, port: int, queue: asyncio.Queue):
@@ -33,7 +43,7 @@ async def read_msgs(host: str, port: int, queue: asyncio.Queue):
             phrase = await reader.readline()
             stamped_phrase = add_timestamp(phrase)
             queue.put_nowait(stamped_phrase)
-
+            await saving_queue.put(stamped_phrase)
 
 
 async def main():
@@ -44,16 +54,19 @@ async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', dest='host', type=str, help='Host name')
     parser.add_argument('--port_read', dest='port_read', type=int, help='Read port number')
+    parser.add_argument('--history', dest='filepath', type=str, help='File to save history')
     args = parser.parse_args()
 
     host = args.host or env.str('HOST', 'minechat.dvmn.org')
     port_read = args.port_read or env.int('PORT_READ', 5000)
+    filepath = args.filepath or env.str('FILEPATH', 'chat.history')
 
     loop = await asyncio.gather(
         gui.draw(messages_queue, sending_queue, status_updates_queue),
-        greeting(messages_queue),
-        generate_msgs(messages_queue),
+        load_history(filepath, messages_queue),
+        # generate_msgs(messages_queue),
         read_msgs(host, port_read, messages_queue),
+        save_messages(filepath, saving_queue),
     )
 
     loop.run_until_complete(gui.draw(messages_queue, sending_queue, status_updates_queue))
